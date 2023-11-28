3e93ff2192a88cd9b19b17cf20d5fad243673af4
alessio.cassarino <alessio.cassarino632@gmail.com>
2023-11-28 16:52:51 +0100

    prova

--- src/main/java/com/example/jwtAuthentication/controller/UserController.java
+++ src/main/java/com/example/jwtAuthentication/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
@@ -24,6 +25,7 @@ public class UserController {
 
     @Autowired
     private AuthenticationManager authenticationManager;
+
 
     @GetMapping("/welcome")
     public String welcome() {
